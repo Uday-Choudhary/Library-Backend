fbd5dc3b0627ad0cf0557378cc6718b0ff34d158
Uday-Choudhary <choudharyuday85@gmail.com>
2025-11-26 09:43:46 +0530

    created-genre-put

--- src/controllers/genre.controllers.js
+++ src/controllers/genre.controllers.js
@@ -1,5 +1,9 @@
 const prisma = require('../../prisma')
 
+// exports.GetGenre = async (req , res) => {
+
+// }
+
 exports.CreateGenre = async (req, res) => {
 
     try {
@@ -24,3 +28,29 @@ exports.CreateGenre = async (req, res) => {
 }
 
 
+exports.UpdateGenre = async (req , res) => {
+    
+    try{
+        const { genreId , name } = req.body 
+        if (!genreId || !name) {
+            return res.status(400).json({ error: 'Missing fields' });
+        }
+
+        const updatedGenre = await prisma.genre.update({
+            where : {
+                id : genreId
+            } ,
+            data : {
+                name : name
+            }
+        })
+
+        return res.status(200).json({ status: 'Updated', updatedGenre })
+
+
+    }catch(error){
+        console.log(error)
+    }
+    
+    
+}
--- src/routes/genre.routes.js
+++ src/routes/genre.routes.js
@@ -1,7 +1,8 @@
 const express = require('express')
-const { CreateGenre } = require('../controllers/genre.controllers')
+const { CreateGenre, UpdateGenre } = require('../controllers/genre.controllers')
 const router = express.Router()
-
+router.get('/:id')
 router.post('/create', CreateGenre)
+router.put('/update', UpdateGenre)
 
 module.exports = router
